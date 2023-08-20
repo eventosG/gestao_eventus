fa5f6dccc726601b478e54f6b08fc93c2e9e7418
gestao_eventus <systemsmanagerlda@gmail.com>
2023-08-20 07:50:26 +0100

    Mensagem

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,11 +1,13 @@
 "use client";
 import React from 'react';
 import { SessionProvider } from 'next-auth/react';
+import Script from 'next/script'
 
 
 function Provider({ children, session }) {
   return (
     <SessionProvider session={session}>
+      <Script src='./script.js' />
       {children}
     </SessionProvider>
   )
