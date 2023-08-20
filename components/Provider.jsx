f66d1a0dad7116c26ac3a0a0ab45c5d7ab042ef5
gestao_eventus <systemsmanagerlda@gmail.com>
2023-08-20 08:04:36 +0100

    Mensagem

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,13 +1,16 @@
 "use client";
 import React from 'react';
 import { SessionProvider } from 'next-auth/react';
-import Script from 'next/script'
+import Script from 'next/script';
+import Head from "next/head";
 
 
 function Provider({ children, session }) {
   return (
     <SessionProvider session={session}>
-      <Script src='./script.js' />
+      <Head>
+        <Script src='/script.js' />
+      </Head>      
       {children}
     </SessionProvider>
   )
--- components/script.js
+++ components/script.js
@@ -3,5 +3,6 @@ window.$crisp=[];
         (function(){d=document;
         s=d.createElement("script");
         s.src="https://client.crisp.chat/l.js";
-        s.async=1;d.getElementsByTagName("head")[0].appendChild(s);
+        s.async=1;
+        d.getElementsByTagName("head")[0].appendChild(s);
         })();
