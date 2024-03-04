a6e8b0f8547707f419acc297d1b49f2dc071f974
eventosG <lurdino94@gmail.com>
2024-03-04 04:25:07 +0000

    Footer edition

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,15 +4,15 @@ function Footer() {
   return (
     <>
       <footer>
-        <div class="container">
-            <div class="aboutus">
+        <div className="container">
+            <div className="aboutus">
             <h2>About Us</h2>
             <p className='text-black text-lg'>
               Nos somos o parceiro ideal na organizacao do seu Evento, temos solucoes a medida das suas necessidades, 
               simplificando processos, proporcionando conforto e elegancia que a organizacao do seu Evento merece...
             </p>
             </div>
-            <div class="link-supp">
+            <div className="link-supp">
             <h2>Support</h2>
             <ul>
                 <li><a href="">FAQ</a></li>
@@ -20,7 +20,7 @@ function Footer() {
                 <li><a href="">Help</a></li>
             </ul>
             </div>
-            <div class="link-category">
+            <div className="link-category">
             <h2>Shop</h2>
             <ul>
                 <li><a href="">Men</a></li>
@@ -28,37 +28,37 @@ function Footer() {
                 <li><a href="">Kids</a></li>
             </ul>
             </div>
-            <div class="link-contact">
+            <div className="link-contact">
             <h2>Contact Us</h2>
-            <ul class="cont">
+            <ul className="cont">
                 <li>
-                <span><i class="fa-solid fa-phone"></i></span><p><a href="#">+258 84 628 6567</a></p>
-                <span><i class="fa-solid fa-envelope"></i></span><p><a href="#">hiventos@gmail.com</a></p>
+                <span><i className="fa-solid fa-phone"></i></span><p><a href="#">+258 84 628 6567</a></p>
+                <span><i className="fa-solid fa-envelope"></i></span><p><a href="#">hiventos@gmail.com</a></p>
                 </li>
             </ul>
             </div>
         </div>
-        <div className='flex justify-center'>
-              <ul class="wrapper">
-                <li class="icon facebook">
-                  <span class="tooltip">Facebook</span>
-                  <span><i class="fab fa-facebook-f"></i></span>
+        {/* <div className='flex justify-center'>
+              <ul className="wrapper">
+                <li className="icon facebook">
+                  <span className="tooltip">Facebook</span>
+                  <span><i className="fab fa-facebook-f"></i></span>
                 </li>
-                <li class="icon twitter">
-                  <span class="tooltip">TikTok</span>
-                  <span><i class="fa-brands fa-tiktok"></i></span>
+                <li className="icon twitter">
+                  <span className="tooltip">TikTok</span>
+                  <span><i className="fa-brands fa-tiktok"></i></span>
                 </li>
-                <li class="icon instagram">
-                  <span class="tooltip">Instagram</span>
-                  <span><i class="fab fa-instagram"></i></span>
+                <li className="icon instagram">
+                  <span className="tooltip">Instagram</span>
+                  <span><i className="fab fa-instagram"></i></span>
                 </li>
-                <li class="icon youtube">
-                  <span class="tooltip">WhatsApp</span>
-                  <span><i class="fa-brands fa-whatsapp"></i></span>
+                <li className="icon youtube">
+                  <span className="tooltip">WhatsApp</span>
+                  <span><i className="fa-brands fa-whatsapp"></i></span>
                 </li>
               </ul>
-            </div>
-        <p class="footer-title">Copyrights © <span>Gerstao de Ventos</span></p>
+            </div> */}
+        <p className="footer-title">Copyrights © <span>Gerstao de Ventos</span></p>
     </footer>
     </> 
   )
